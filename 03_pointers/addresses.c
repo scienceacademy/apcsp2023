6b61bf8596da8802a2e6fb3e7ee12fdbd09a9efe
Chris Bradfield <cb@scribe.net>
2023-10-05 21:15:43 -0700

    updates

---
+++ 03_pointers/addresses.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = "HI!";
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%c\n", *(s+3));
+    printf("%c\n", *(s+50000));
+}
